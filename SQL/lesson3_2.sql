a65809126da5f15cd46df68f871ed7f16ffc375c
楊智筑 <161096347+SandyYCH@users.noreply.github.com>
2024-03-02 11:41:35 +0800

    Update lesson3_2.sql

---
+++ SQL/lesson3_2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS student(
+	student_ID SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	major VARCHAR(20)
+);
+	
+DROP TABLE student
+
+CREATE TABLE IF NOT EXISTS artists(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR
+);
+
+DROP TABLE artists
+
+SELECT *
+FROM artists; 
