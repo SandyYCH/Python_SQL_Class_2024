644cd8a0514bc9014056f4ef1c88f2290959a536
楊智筑 <161096347+SandyYCH@users.noreply.github.com>
2024-04-02 18:03:45 +0800

    Update GROUP BY,HAVING應用.sql

--- SQL/GROUP BY,HAVING應用.sql
+++ SQL/GROUP BY,HAVING應用.sql
@@ -1,11 +1,22 @@
 /*全省各站點2022年進站總人數*/
-SELECT EXTRACT(YEAR FROM 日期),名稱,
+SELECT EXTRACT(YEAR FROM 日期)AS 年,名稱,
 SUM(進站人數) AS 總和 
 FROM gate_count LEFT JOIN stations ON gate_count.站點編號=stations.編號
 WHERE (日期 BETWEEN '2022-01-01' AND '2022-12-31')
 GROUP BY EXTRACT(YEAR FROM 日期),名稱
+ORDER BY 總和 DESC
+
 
 /*全省各站點2022年進站總人數大於5佰萬人的站點*/
+SELECT EXTRACT(YEAR FROM 日期)AS 年,名稱,
+SUM(進站人數) AS 總和 
+FROM gate_count LEFT JOIN stations ON gate_count.站點編號=stations.編號
+WHERE (日期 BETWEEN '2022-01-01' AND '2022-12-31')
+GROUP BY EXTRACT(YEAR FROM 日期),名稱
+HAVING SUM(進站人數) > 5000000
+ORDER BY 總和 DESC
+
+
 /*基隆火車站2020年,每月份進站人數*/
 /*基隆火車站2020年,每月份進站人數,由多至少*/
 /*基隆火車站2020,2021,2022,每年進站人數*/
