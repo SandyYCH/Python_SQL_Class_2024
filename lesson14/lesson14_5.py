3ec215ad24d922a6a8591bb464b9c4bc8c3b2840
Sandy <seekcandy@gmail.com>
2024-04-19 17:57:07 +0800

    Update lesson14_5.py

--- lesson14/lesson14_5.py
+++ lesson14/lesson14_5.py
@@ -4,6 +4,7 @@ import psycopg2
 import os
 load_dotenv()
 
+#建立資料表
 @st.cache_resource
 def get_contacts() -> list:
     with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
@@ -18,7 +19,8 @@ def get_contacts() -> list:
             for item in datas:
                 contacts.append({'id':item[0],'客戶名稱':item[1],'姓名':item[2],'電話':item[3],'郵件':item[4]})
             return contacts
-        
+
+#建立下拉式選單 
 @st.cache_resource
 def get_names() -> list:
     with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
@@ -33,9 +35,7 @@ def get_names() -> list:
             for item in datas:
                 names.append(item[0])
             return names
-            
-
-
+        
 with st.sidebar:
     option = st.selectbox("請輸入客戶",get_names())
     st.write('You selected:', option)
