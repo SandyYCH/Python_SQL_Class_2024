19391d8d1e453383aa00edec10b538d6a252f29e
Sandy <seekcandy@gmail.com>
2024-04-23 11:54:17 +0800

    Update lesson15_3.py

--- lesson15/lesson15_3.py
+++ lesson15/lesson15_3.py
@@ -10,4 +10,8 @@ if 'a2' not in st.session_state:
 if 'a3' not in st.session_state:
     st.session_state['a3'] = 300
 
+#命令不會重複被執行。若字典已建立，則命令不會再被執行一次
+if 'a1' not in st.session_state:
+    st.session_state['a1'] = 500
+
 st.write(st.session_state)
