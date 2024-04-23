ba7d8969940a6fdf9e1fdd2350f81a52bef401b8
Sandy <seekcandy@gmail.com>
2024-04-23 16:58:16 +0800

    Update lesson16_2.py

--- lesson16/lesson16_2.py
+++ lesson16/lesson16_2.py
@@ -1,18 +1,19 @@
 import streamlit as st
 
-st.title("Session 和 Callback")
+st.title("Session_state 和 Callback")
 
-col1, buff, col2 = st.columns([2, 1, 2])
+col1, buf, col2 = st.columns([2, 1, 2])
 
-#on_change執行function，這function必須在指令on_change上方
+#on_change執行回調函式(執行回調函式後，重新由上而下執行所有程式)，因此回調函式必須在指令on_change上方
 def lbs_to_kg():
     st.session_state.kg = st.session_state.lbs / 2.2046
 
 def kg_to_lbs():
     st.session_state.lbs = st.session_state.kg * 2.2046
 
+#利用Callback邏輯，Button不用傳出值也可運行
 with col1:
-    st.number_input("磅:",key='lbs',on_change=lbs_to_kg)
+    st.number_input("磅:", key = "lbs", on_change = lbs_to_kg)    #on_change = lbs_to_kg：登記/註冊回調函式
 
 with col2:
-    st.number_input("公斤",key="kg",on_change=kg_to_lbs)
+    st.number_input("公斤:", key = "kg", on_change = kg_to_lbs)
