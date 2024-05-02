09e7823561a1c97559fc7915589cdaa16ab59e6a
Sandy <seekcandy@gmail.com>
2024-05-02 11:25:03 +0800

    Update lesson18_1.sql

--- SQL/lesson18_1.sql
+++ SQL/lesson18_1.sql
@@ -36,18 +36,17 @@ ON CONFLICT DO NOTHING;
 SELECT *
 FROM 站點資訊;
 
-SELECT *
+SELECT * 
 FROM youbike;
 
 SELECT count(*) AS 筆數
-FROM youbike
+FROM youbike;
 
-/*先測試這筆,才可以了解下面*/
 SELECT MAX(日期),編號
 FROM youbike
 GROUP BY 編號;
 
-/*取出最新時間youbike資料*/
+/*取出最新時間youbike資料，使用subquery插入指令*/
 SELECT 日期,站點資訊.站點名稱,行政區,站點地址,lat,lng,總車輛,可借,可還,可借,活動
 FROM youbike
 JOIN 站點資訊 ON youbike.編號 = 站點資訊.站點編號
@@ -69,5 +68,5 @@ JOIN 站點資訊 ON youbike.編號 = 站點資訊.站點編號
 WHERE (日期,編號) IN (
 	SELECT MAX(日期),編號
 	FROM youbike
-	GROUP BY 編號
-) AND 行政區 = '大安區';
+	GROUP BY 編號) 
+	AND 行政區 = '大安區';
